93c202f8257ca06e54dbbec5f7ed46849b6f75b7
Tomohito YABU <tyabu1212@gmail.com>
2022-07-13 22:55:22 +0900

    apply 後にインスタンスのIPアドレスを表示するように変更

--- main.tf
+++ main.tf
@@ -66,3 +66,15 @@ module "ec2_bench" {
   ec2_instance_count = 1
   ec2_volume_size    = var.bench.ec2_volume_size
 }
+
+output "webapp_public_ip" {
+  value = module.ec2_webapp.instance_public_id_addr
+}
+
+output "webapp_private_ip" {
+  value = module.ec2_webapp.instance_private_id_addr
+}
+
+output "bench_public_ip" {
+  value = module.ec2_bench.instance_public_id_addr
+}
--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,3 +1,11 @@
 output "aws_instance" {
   value = aws_instance.participant-instance.*.id
 }
+
+output "instance_public_id_addr" {
+  value = aws_instance.participant-instance.*.public_ip
+}
+
+output "instance_private_id_addr" {
+  value = aws_instance.participant-instance.*.private_ip
+}
